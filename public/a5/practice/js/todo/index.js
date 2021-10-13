8ef966bc3ba24093033ead6a12b48abd0c69ff6e
SkarnerV <skarner.han@gmail.com>
2021-10-12 22:51:32 -0400

    a5

--- public/a5/practice/js/todo/index.js
+++ public/a5/practice/js/todo/index.js
@@ -1,7 +1,5 @@
-import TodoList from "./TodoList";
-
+import TodoList from "./TodoList.js";
 alert('Hello World!');
-
 $('#wd-todo').append(`
     <div class="container">
         <h1>Todo example</h1>
