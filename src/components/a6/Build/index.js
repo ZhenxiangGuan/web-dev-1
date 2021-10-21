d6724c8d545d6448c876647d04a5b2372d1778b8
SkarnerV <skarner.han@gmail.com>
2021-10-21 01:03:20 -0400

    a6

---
+++ src/components/a6/Build/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+const Build = () => {
+    return(
+    <>
+        
+        <h1>Build</h1>
+        <Link to="/a6/practice">
+        Practice
+        </Link> |
+        <Link to="/a6/build">
+        Build
+        </Link>        
+    </>
+        
+    )
+};
+
+export default Build;
